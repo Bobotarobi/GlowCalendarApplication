11869fbfe3de7c4ed4d3af657a1cc306c81f19de
Maks Balamut <balamutmaks@gmail.com>
2025-02-05 16:05:31 +0200

    Changed directory of user service to services/user. Changed directory of register.request.ts

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {RegisterRequest} from './UserRequest/register.request';
+import {RegisterRequest} from './requests/register.request';
 
 @Injectable({
   providedIn: 'root'
